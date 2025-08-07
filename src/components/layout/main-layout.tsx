d13ee14db5f4f8d04fd0fb89c05cf0361b7e478f
Hakha Shojai <itmanishere@gmail.com>
2025-08-07 08:38:33 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div style="backgr

---
+++ src/components/layout/main-layout.tsx
@@ -0,0 +1,20 @@
+import Footer from "@/components/layout/footer";
+import Header from "@/components/layout/header";
+import CtaBanner from "@/components/layout/cta-banner";
+
+export default function MainLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow">{children}</main>
+      <div className="mt-8">
+        <CtaBanner />
+      </div>
+      <Footer />
+    </div>
+  );
+}
